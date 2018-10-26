ec6c10edeffabc840a25f92f2c51bb102deb4e1f
chenyu(May) sun <35646043+mayhhhhh@users.noreply.github.com>
2018-10-26 00:16:38 -0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -18,15 +18,12 @@ using namespace std;
 //Client side
 int main(int argc, char *argv[])
 {
-    //we need 2 things: ip address and port number, in that order
     if(argc != 3)
     {
         cerr << "Usage: ip_address port" << endl; exit(0); 
-    } //grab the IP address and port number 
+    }
     char *serverIp = argv[1]; int port = atoi(argv[2]); 
-    //create a message buffer 
     char msg[1500]; 
-    //setup a socket and connection tools 
     struct hostent* host = gethostbyname(serverIp); 
     sockaddr_in sendSockAddr;   
     bzero((char*)&sendSockAddr, sizeof(sendSockAddr)); 
@@ -35,7 +32,6 @@ int main(int argc, char *argv[])
         inet_addr(inet_ntoa(*(struct in_addr*)*host->h_addr_list));
     sendSockAddr.sin_port = htons(port);
     int clientSd = socket(AF_INET, SOCK_STREAM, 0);
-    //try to connect...
     int status = connect(clientSd,
                          (sockaddr*) &sendSockAddr, sizeof(sendSockAddr));
     if(status < 0)
@@ -52,7 +48,7 @@ int main(int argc, char *argv[])
         cout << ">";
         string data;
         getline(cin, data);
-        memset(&msg, 0, sizeof(msg));//clear the buffer
+        memset(&msg, 0, sizeof(msg));
         strcpy(msg, data.c_str());
         if(data == "exit")
         {
@@ -61,7 +57,7 @@ int main(int argc, char *argv[])
         }
         bytesWritten += send(clientSd, (char*)&msg, strlen(msg), 0);
         cout << "Awaiting server response..." << endl;
-        memset(&msg, 0, sizeof(msg));//clear the buffer
+        memset(&msg, 0, sizeof(msg));
         bytesRead += recv(clientSd, (char*)&msg, sizeof(msg), 0);
         if(!strcmp(msg, "exit"))
         {
